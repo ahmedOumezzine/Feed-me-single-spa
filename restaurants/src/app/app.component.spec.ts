820fdd2a556e4adc1f98ea725f53074eec1e9a76
ahmedoumezzine <ahmedoumezzine>
2023-08-12 17:52:17 -0400

    revue code

--- restaurants/src/app/app.component.spec.ts
+++ restaurants/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyFirstApp'`, () => {
+  it(`should have as title 'restaurants'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyFirstApp');
+    expect(app.title).toEqual('restaurants');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyFirstApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('restaurants app is running!');
   });
 });
